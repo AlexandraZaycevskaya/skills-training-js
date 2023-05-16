4831ee37306a55fc992c3a7e23086b9867499aa6
AlexandraZaycevskaya <126073816+AlexandraZaycevskaya@users.noreply.github.com>
2023-05-17 00:22:21 +0300

    Add files via upload

---
+++ second.js
@@ -0,0 +1,17 @@
+const sum = 100 + 100 * 3 - 4 / 2;
+console.log(sum);
+
+const a = 5;
+const b = 3;
+const c = a + b;
+console.log(c);
+
+const RUB_USD_CURRENCY_RATE = 0.025;
+const RUB = 10000;
+const USD = RUB_USD_CURRENCY_RATE * RUB;
+console.log(USD);
+
+let counter = 0;
+counter = counter + 1;
+++counter;
+console.log(counter);
